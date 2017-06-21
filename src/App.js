70fe5569ec82cd3532948b2e7963bb7fcbd04d17
Lex Anderson <lexanderson1114@gmail.com>
2017-06-21 13:06:30 -0400

    Updated to the NASA API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route, NavLink } from 'react-router-dom'
+import Github from './Github'
+import Nasa from './Nasa'
 import './App.css';
 
 class App extends Component {
@@ -8,17 +10,21 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <div className="App-headings">
-          <h3> Aint no party like an</h3>
-          <h1>API Party </h1>
-        </div>
-        <ul className="nav-links">
-          <li>
-            <NavLink to='/github'> Github API </NavLink>
-          </li>
-        </ul>
+            <h3>Ain't no party like an</h3>
+            <h1>API Party</h1>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <NavLink to={'/github'}>Github API</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/nasa'}>NASA API</NavLink>
+            </li>
+          </ul>
         </div>
         <Switch>
-          <Route path='/github' render={() => <h1>Github</h1>} />
+          <Route path='/github' component={Github} />
+          <Route path='/nasa' component={Nasa} />
           <Route render={() => <p>To get started, click one of the links above</p>} />
         </Switch>
       </div>
